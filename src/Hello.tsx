8fae784c9519579a1cb4ae118fcd2eec7177e8a2
issiy <allmylifewriter@gmail.com>
2021-11-06 23:29:15 +0900

    ルーティングで画面表示#3

--- src/Hello.tsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-
-const Hello = () => <h1>こんにちは！ 石田です！</h1>;
-export default Hello;
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: path.resolve(__dirname, 'src/app.tsx'),
+  entry: path.resolve(__dirname, 'src/index.tsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.js',
